3b581bda6dda045032b88e1e5b800b20b0b506db
SKoonkor <suttikoon.koonkor@outlook.com>
2026-04-01 10:53:49 +0700

    Added CSP coeffs calculator

---
+++ src/galspectra/csp/builder.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def build_csp_coefficients(
+        ssp_age,
+        ssp_coeffs,
+        target_ages,
+        mass_weights
+        ):
+    """
+    Build CSP in PCA space.
+
+    Parameters
+    ----------
+    ssp_age: (N_ssp, )
+    ssp_coeffs : (N_ssp, N_pc)
+    target_ages : (N_bin, )
+        Ages corresponding to each mass bin
+    mass_weights : (N_bin, )
+
+    Returns
+    -------
+    csp_coeffs : (N_pc, )
+    """
+
+    n_pc = ssp_coeffs.shape[1]
+    csp = np.zeros(n_pc)
+
+    for age, mass in zip(target_ages, mass_weights):
+        # Find nearest SSP
+        ## Will need to be updated
+        ##   - to interpolation in age
+        ##   - to interpolation in age and metallicity 
+        idx = np.argmin(np.abs(ssp_ages - age))   
+        
+
+        csp += mass * ssp_coeffs[idx]
+
+    return csp
