ab5b451b0db9b63a3bce080a51ee05fa6221f315
SKoonkor <suttikoon.koonkor@outlook.com>
2026-09-21 19:36:31 +0700

    Edited param grid

--- src/galspectra/sampling/paramgrid.py
+++ src/galspectra/sampling/paramgrid.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.stats import qmc
 from itertools import product
 
-# Validation
 def _validate_params(params):
     for p in params:
         if "name" not in p: raise ValueError("Each parameter must have a 'name'")
@@ -11,7 +10,6 @@ def _validate_params(params):
         if p["spacing"] not in ["linear", "log"]:
             raise ValueError(f"{p['name']}: spacing must be 'linear' or 'log'")
 
-# Grid Generators
 def generate_lhs_grid(params, n_samples=200, seed=None):
     """
     Generate parameter samples using Latin Hypercube Sampling.
@@ -28,27 +26,17 @@ def generate_lhs_grid(params, n_samples=200, seed=None):
 
     scaled = qmc.scale(unit_sample, mins, maxs)
 
-    # Apply scaling
     for i, p in enumerate(params):
         if p["spacing"] == "log":
             scaled[:, i] = 10**scaled[:, i]
 
-    # return scaled
     return {
             "samples": scaled,
             "param_names": [p["name"] for p in params]
             }
 
 def generate_cartesian_grid(params, grid_sizes):
-    """
-    Generate full Cartesian grid of parameters.
-
-    Parameters
-    ----------
-    params: list of dict
-    grid_sizes : list of int
-        Number of grid points per parameter
-    """
+    """Full Cartesian grid of parameters. grid_sizes: points per parameter."""
     _validate_params(params)
 
     if len(params) != len(grid_sizes):
@@ -64,10 +52,8 @@ def generate_cartesian_grid(params, grid_sizes):
 
         value_axes.append(axis)
 
-    # Cartesian product (N-dim)
-    grid = np.array(list(product(*value_axes)))
+    grid = np.array(list(product(*value_axes)))  # Cartesian product (N-dim)
 
-    # return grid
     return {
             "samples": grid,
             "param_names": [p["name"] for p in params]
@@ -76,27 +62,3 @@ def generate_cartesian_grid(params, grid_sizes):
 
 
 
-
-
-
-
-# Test ZONE
-# 
-# params = [
-#         {"name": "tage", "min": 1e-4, "max": 13.8, "spacing": "linear"},
-#         {"name": "Z", "min": 0.012, "max": 0.03, "spacing": "linear"},
-#         ]
-# 
-# scaled_lhs = generate_lhs_grid(params=params, n_samples=10)
-# scaled_cartesian = generate_cartesian_grid(params=params, grid_sizes = [5, 2])
-# 
-# print ("\nLHS")
-# print (scaled_lhs)
-# print ("\nCartesian")
-# print (scaled_cartesian)
-# 
-# 
-# 
-# 
-# scaled_lhs = []
-# scaled_cartesian = []
